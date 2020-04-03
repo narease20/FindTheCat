d00094e2d1ca8f612caf66e6aacdd68e4fa3a6ee
narease20 <narease20@transy.edu>
2020-04-03 00:23:18 -0400

    Working towards making Wrath stop at walls.

--- Untitled Cat Game/Assets/LeanTween/Examples/Scenes/FollowPlayer.cs
+++ Untitled Cat Game/Assets/LeanTween/Examples/Scenes/FollowPlayer.cs
@@ -6,12 +6,17 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform mTarget;
 
-    float mSpeed = 5.0f;
+    [SerializeField, Range(0.1f, 10f)]
+    public float mSpeed = 5.0f;
 
-    
+    public bool following = true;
+    public SphereCollider burnRange;
+    [SerializeField, Range(0.1f, 10f)]
+    public float burnTimer;
+    public FireWall closestFireWall;
 
     // make a constant for wrath to stop at a certain distance
-    const float EPSILON = 1.0f;
+    public float EPSILON = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +27,68 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (following)
+        {
+            FollowTarget();
+        }
+
+    }
+
+    public void FollowTarget()
+    {
         /* LookAt makes Wrath look at the player. 
-         * May need to erase this part because it will probably rotate his whole body. */
-        transform.LookAt(mTarget.position, Vector3.up);
+        * May need to erase this part because it will probably rotate his whole body. */
+        transform.LookAt(new Vector3 (mTarget.position.x, 0, mTarget.position.z), Vector3.up);
         //zRot = Mathf.Clamp()
 
 
-     //    Vector3 difference = mTarget.position - transform.position;
-    //    float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-     //   float rotationX = Mathf.Atan2(difference.z, difference.y) * Mathf.Rad2Deg;
-     //  transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        //    Vector3 difference = mTarget.position - transform.position;
+        //    float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //   float rotationX = Mathf.Atan2(difference.z, difference.y) * Mathf.Rad2Deg;
+        //  transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
         /* this part should make wrath follow position. */
         if ((transform.position - mTarget.position).magnitude > EPSILON)
             transform.Translate(0.0f, 0.0f, mSpeed * Time.deltaTime);
+    }
+
+    /* Doesnt Work
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FireWall")
+        {
+            closestFireWall = other.GetComponent<FireWall>();
+            Debug.Log("Got a wall");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (closestFireWall && !(other.gameObject.tag == "FireWall"))
+        {
+            closestFireWall = null;
+            Debug.Log("removed a wall");
+        }
+    }
+    */
+
+    public void BushFinder()
+    {
+        following = false;
+        // Check around for bush then call its function to destory it.
+        StopAllCoroutines();
+        StartCoroutine(WallWait());
+        if (closestFireWall)
+        {
+            closestFireWall.DestroyMe();
+        }
+
+        following = true;
+    }
+
+    IEnumerator WallWait()
+    {
+        transform.LookAt(new Vector3(closestFireWall.transform.position.x, 0, closestFireWall.transform.position.z), Vector3.up);
+        yield return new WaitForSeconds(burnTimer);
+        //transform.LookAt(new Vector3(closestFireWall.transform.position.x, 0, closestFireWall.transform.position.z), Vector3.up);
     }
 }
