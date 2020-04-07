fd99bd9c176708b67feb19b24bb2899ce5dbccd4
senance20 <senance20@transy.edu>
2020-04-07 03:24:45 -0400

    Edited wrath maze and cat
    added cat trail to wrath scene, tried to add textured cat but failed.

--- Untitled Cat Game/Assets/CatManager.cs
+++ Untitled Cat Game/Assets/CatManager.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+
+/* This block of comments is because it's 2am and I don't know how to code. 
+ * First, make a checker for if the cat has been pet. 
+ * put the whole dolly cart movement system inside that check.
+ * If the cat has been pet, THEN it starts to follow the path. 
+ * But it needs to trigger a couple of animations first?? like, sit->stand->walk.
+ * THose could be on a short timer, I guess.
+ * I'm sorry. I tried to help. I failed. I need sleep. 
+ * /
+ */
+
 public class CatManager : MonoBehaviour
 {
     public bool followingPath;
@@ -12,7 +23,7 @@ public class CatManager : MonoBehaviour
     public CinemachineSmoothPath beginningTrack;
     public Transform parentObject;
 
-    public Animator anim;
+    public Animator anim; 
 
 
     // Start is called before the first frame update
