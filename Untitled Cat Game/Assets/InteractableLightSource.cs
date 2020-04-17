a24c5e604396524141cd7d31265d1d6959940b32
narease20 <narease20@transy.edu>
2020-04-17 16:34:03 -0400

    Darkness inprison me no more
    Figured out how to affect the material shader and the light source.

--- Untitled Cat Game/Assets/InteractableLightSource.cs
+++ Untitled Cat Game/Assets/InteractableLightSource.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Light))]
 public class InteractableLightSource : Interactable
 {
-    Light lightSource;
+    public Light lightSource;
     public float lightRange = 6;
     public float lightIntensity = 2;
 
@@ -15,55 +15,63 @@ public class InteractableLightSource : Interactable
     public float powerTime = 2;
     public float poweredLightRange;
     public float poweredLightIntensity;
-    bool currentlyPowered = false;
+    //bool currentlyPowered = false;
+
+    public Material fog;
+    public float fBase;
+    public float fReduction;
+
+    //float tempRange = 0;
+    //float tempInten = 0;
+    //float tempFRedux = 0;
 
     private void Start()
     {
-        lightSource = GetComponent<Light>();
+        fBase = fog.GetFloat("_Strength");
+        fReduction = fBase / powerMultiply;
+
         lightRange = lightSource.range;
         lightIntensity = lightSource.intensity;
 
         poweredLightRange = lightRange * powerMultiply;
         poweredLightIntensity = lightIntensity * powerMultiply;
+
+        //Mathf.Clamp(tempRange, lightRange, poweredLightRange);
+        //Mathf.Clamp(tempInten, lightIntensity, poweredLightIntensity);
     }
 
-    public void Update()
-    {
+    public override void Update()
+    { 
         /*
-        if (currentlyPowered)
+        if (currentlyPowered && tempRange < poweredLightRange && tempInten < poweredLightIntensity)
         {
-            lightSource.range = poweredLightRange;
-            lightSource.intensity = poweredLightIntensity;
+            lightSource.range = tempRange += Time.deltaTime;
+            lightSource.intensity = tempInten += Time.deltaTime;
+            fReduction += Time.deltaTime;
+            fog.SetFloat("_Strength", fReduction);
         }
-        if (!currentlyPowered)
+        if (!currentlyPowered && tempRange > lightRange && tempInten > lightIntensity)
         {
-            lightSource.range = lightRange;
-            lightSource.intensity = lightIntensity;
-        }
-
-        
-        if (currentlyPowered && lightSource.range < poweredLightRange && lightSource.intensity < poweredLightIntensity)
-        {
-            lightSource.range += Time.deltaTime;
-            lightSource.intensity += Time.deltaTime;
-        }
-        if (!currentlyPowered && lightSource.range < lightRange && lightSource.intensity < lightIntensity)
-        {
-            lightSource.range -= Time.deltaTime;
-            lightSource.intensity -= Time.deltaTime;
+            lightSource.range = tempRange -= Time.deltaTime;
+            lightSource.intensity = tempInten -= Time.deltaTime;
+            fReduction -= Time.deltaTime;
+            fog.SetFloat("_Strength", fReduction);
         }
         */
     }
 
+
     public override void Action()
     {
-        //base.Action();
-        LightTimer();
+        StartCoroutine(LightTimer());
+        //Debug.Log("HEY DOM");
+        return;
     }
 
     //Doesnt work. Just edit the shader's power instead
     IEnumerator LightTimer()
     {
+        fog.SetFloat("_Strength", fReduction);
         lightSource.range = poweredLightRange;
         lightSource.intensity = poweredLightIntensity;
         //currentlyPowered = true;
@@ -71,5 +79,6 @@ public class InteractableLightSource : Interactable
         //currentlyPowered = false;
         lightSource.range = lightRange;
         lightSource.intensity = lightIntensity;
+        fog.SetFloat("_Strength", fBase);
     }
 }
